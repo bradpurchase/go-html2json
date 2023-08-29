795013d469a30ccc8f58451b30422b9072777ae6
Brad Purchase <5716+bradpurchase@users.noreply.github.com>
2023-08-29 09:37:52 -0400

    chore: update root filename

---
+++ main.go
@@ -0,0 +1,60 @@
+package html2json
+
+import (
+	"encoding/json"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type HTMLElement struct {
+	Tag      string            `json:"tag"`
+	Attrs    map[string]string `json:"attrs"`
+	Children []*HTMLElement    `json:"children"`
+}
+
+func parseNode(node *html.Node) *HTMLElement {
+	if node.Type == html.DocumentNode {
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			return parseNode(child)
+		}
+		return nil
+	} else if node.Type == html.ElementNode {
+		element := &HTMLElement{
+			Tag:   node.Data,
+			Attrs: make(map[string]string),
+		}
+		for _, attr := range node.Attr {
+			if attr.Key == "id" || attr.Key == "class" || attr.Key == "style" {
+				continue
+			}
+			element.Attrs[attr.Key] = attr.Val
+		}
+		for child := node.FirstChild; child != nil; child = child.NextSibling {
+			element.Children = append(element.Children, parseNode(child))
+		}
+		return element
+	} else if node.Type == html.TextNode {
+		return &HTMLElement{
+			Tag:      "text",
+			Attrs:    map[string]string{"text": node.Data},
+			Children: []*HTMLElement{{Tag: "", Attrs: nil, Children: nil}},
+		}
+	} else {
+		return nil
+	}
+}
+
+func Convert(htmlString string) (string, error) {
+	doc, err := html.Parse(strings.NewReader(htmlString))
+	if err != nil {
+		return "", err
+	}
+	element := parseNode(doc)
+
+	jsonBytes, err := json.Marshal(element)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonBytes), nil
+}
---
+++ main_test.go
@@ -0,0 +1,58 @@
+package html2json
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestConvert(t *testing.T) {
+	html := "<html><head></head><body><h1>Hello World</h1></body></html>"
+	jsonString, err := Convert(html)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := `{"tag":"html","attrs":{},"children":[{"tag":"head","attrs":{},"children":null},{"tag":"body","attrs":{},"children":[{"tag":"h1","attrs":{},"children":[{"tag":"text","attrs":{"text":"Hello World"},"children":[{"tag":"","attrs":null,"children":null}]}]}]}]}`
+	if jsonString != expected {
+		t.Errorf("got %s, want %s", jsonString, expected)
+	}
+}
+
+func TestConvert_StripsIDAttr(t *testing.T) {
+	html := "<html><head></head><body><h1 id='fancy-heading'>Hello World</h1></body></html>"
+	jsonString, err := Convert(html)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := `{"tag":"html","attrs":{},"children":[{"tag":"head","attrs":{},"children":null},{"tag":"body","attrs":{},"children":[{"tag":"h1","attrs":{},"children":[{"tag":"text","attrs":{"text":"Hello World"},"children":[{"tag":"","attrs":null,"children":null}]}]}]}]}`
+	if strings.Contains(jsonString, "fancy-heading") {
+		t.Errorf("got %s, want %s", jsonString, expected)
+	}
+}
+
+func TestConvert_StripsClassAttr(t *testing.T) {
+	html := "<html><head></head><body><h1>Hello World</h1><p class='description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></body></html>"
+	jsonString, err := Convert(html)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := `{"tag":"html","attrs":{},"children":[{"tag":"head","attrs":{},"children":null},{"tag":"body","attrs":{},"children":[{"tag":"h1","attrs":{},"children":[{"tag":"text","attrs":{"text":"Hello World"},"children":[{"tag":"","attrs":null,"children":null}]}]},{"tag":"p","attrs":{},"children":[{"tag":"text","attrs":{"text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit."},"children":[{"tag":"","attrs":null,"children":null}]}]}]}]}`
+	if strings.Contains(jsonString, "description") {
+		t.Errorf("got %s, want %s", jsonString, expected)
+	}
+}
+
+func TestConvert_StripsStyleAttr(t *testing.T) {
+	html := "<html><head></head><body><div style='background: red;'><h1>Hello World</h1></div></body></html>"
+	jsonString, err := Convert(html)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := `{"tag":"html","attrs":{},"children":[{"tag":"head","attrs":{},"children":null},{"tag":"body","attrs":{},"children":[{"tag":"div","attrs":{},"children":[{"tag":"h1","attrs":{},"children":[{"tag":"text","attrs":{"text":"Hello World"},"children":[{"tag":"","attrs":null,"children":null}]}]}]}]}]}`
+	if strings.Contains(jsonString, "style") {
+		t.Errorf("got %s, want %s", jsonString, expected)
+	}
+}
